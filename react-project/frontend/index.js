a88f5dfb5e03de618fd3838bae00a73453e9f11d
Gabriel Cabrejas <gabriel@cabrejas.net>
2023-11-17 17:18:06 +0100

    wip

--- react-project/frontend/index.js
+++ react-project/frontend/index.js
@@ -1,8 +1,8 @@
+import './styles/reset.css'
+import './styles/styles.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
-import './styles/reset.css'
-import './styles/styles.css'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
--- react-project/webpack.config.js
+++ react-project/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 const DEVELOPMENT = 'development'
@@ -6,7 +7,6 @@ const ENV = process.env.NODE_ENV || DEVELOPMENT
 const IS_DEV = ENV === DEVELOPMENT
 
 const HTML_LOADER = 'html-loader'
-const STYLE_LOADER = 'style-loader'
 const CSS_LOADER = 'css-loader'
 const BABEL_LOADER = 'babel-loader'
 const STRING_REPLACE_LOADER = 'string-replace-loader'
@@ -15,11 +15,15 @@ const FILE_LOADER = 'file-loader'
 const SERVER_URL = /http:\/\/localhost:9009/g
 const FRONTEND_PORT = 3003
 
+const DIST_PATH = 'dist'
+const PUBLIC_PATH = IS_DEV ? '/' : './'
 const INDEX_HTML_PATH = './frontend/index.html'
 const INDEX_JS_PATH = './frontend/index.js'
-const DIST_FOLDER = 'dist'
-const BUNDLE_FILE = 'index.js'
-const AUDIO = 'audio/'
+const AUDIO_PATH = 'audio/'
+
+const JS_NAME = IS_DEV ? 'index.js' : 'index.[contenthash].js'
+const CSS_NAME = IS_DEV ? 'style.css' : 'style.[contenthash].css'
+const MP3_NAME = '[name].[ext]'
 
 const SOURCE_MAP = IS_DEV ? 'source-map' : false
 
@@ -27,18 +31,21 @@ const config = {
   entry: INDEX_JS_PATH,
   mode: ENV,
   output: {
-    filename: BUNDLE_FILE,
-    publicPath: '/',
-    path: path.resolve(__dirname, DIST_FOLDER),
+    filename: JS_NAME,
+    publicPath: PUBLIC_PATH,
+    path: path.resolve(__dirname, DIST_PATH),
   },
   devtool: SOURCE_MAP,
   plugins: [
     new HtmlWebpackPlugin({
       template: INDEX_HTML_PATH,
     }),
+    new MiniCssExtractPlugin({
+      filename: CSS_NAME,
+    }),
   ],
   devServer: {
-    static: path.join(__dirname, DIST_FOLDER),
+    static: path.join(__dirname, DIST_PATH),
     historyApiFallback: true,
     compress: true,
     port: FRONTEND_PORT,
@@ -59,7 +66,7 @@ const config = {
       {
         test: /\.css$/i,
         use: [
-          STYLE_LOADER,
+          MiniCssExtractPlugin.loader,
           CSS_LOADER,
         ],
       },
@@ -73,9 +80,9 @@ const config = {
           {
             loader: FILE_LOADER,
             options: {
-              name: '[name].[ext]',
-              outputPath: AUDIO,
-              publicPath: AUDIO,
+              name: MP3_NAME,
+              outputPath: AUDIO_PATH,
+              publicPath: AUDIO_PATH,
             },
           },
         ],
