20f6c866409482e4a7b869c4fe6c4b473e36af97
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-01-17 16:19:35 +0100

    wip

--- react-project/jest.config.js
+++ react-project/jest.config.js
@@ -151,7 +151,7 @@ const config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  testEnvironment: "<rootDir>/__jest__/jsdom.extended.js",
 
   // Options that will be passed to the testEnvironment
   testEnvironmentOptions: {
