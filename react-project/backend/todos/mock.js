b5ee9caef4ae4cfe55bd3c2a83c9f9bf5637c482
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-02-27 15:44:27 +0100

    wip

--- react-project/backend/todos/mock.js
+++ react-project/backend/todos/mock.js
@@ -5,16 +5,20 @@ const { http, HttpResponse } = require('msw')
 function getAll() {
   return HttpResponse.json(Todo.getAll())
 }
+
 function getById({ params }) {
   return HttpResponse.json(Todo.getById(params.id))
 }
+
 async function create({ request }) {
   const todo = Todo.create(await request.json())
   return HttpResponse.json(todo)
 }
+
 function toggleDone({ params }) {
   return HttpResponse.json(Todo.toggleDone(params.id))
 }
+
 function remove({ params }) {
   return HttpResponse.json(Todo.remove(params.id))
 }
