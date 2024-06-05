279fd7b46700296ed2d279a8304a926556964321
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-06-05 20:47:17 +0200

    wip

--- react-project/eslint.config.mjs
+++ react-project/eslint.config.mjs
@@ -2,13 +2,20 @@ import globals from "globals"
 import pluginJs from "@eslint/js"
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
 
-
 export default [
   {
+    files: ["frontend/**/*.js"],
     rules: {
-      "semi": [2, "always"],
+      // "semi": [2, "always"], // You do you!
     },
     languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["backend/**/*.js"],
+    rules: {
+      // "semi": [2, "never"], // You do you!
+    },
+    languageOptions: { globals: globals.commonjs },
   },
   pluginJs.configs.recommended,
   pluginReactConfig,
